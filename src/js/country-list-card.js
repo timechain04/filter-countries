0662df106e2e28bbc197d04ea035483063b396b1
timechain04 <119622380+timechain04@users.noreply.github.com>
2023-06-16 12:18:14 +0000

    Deploying to gh-pages from @ timechain04/filter-countries@9adf1c8e92ab95ad29ac031fb7cebb89c8636060 🚀

--- src/js/country-list-card.js
+++
@@ -1,6 +0,0 @@
-export default function createCountryListCard({ flags, name }) {
-    return `<li class='country-card'>
-              <div class='country-img'><img src='${flags.svg}' alt='${name.official}'/></div>
-              <p class='country-name'>${name.common}</p>
-        </li>`;
-  }

9adf1c8e92ab95ad29ac031fb7cebb89c8636060
Andrii Lutsiak <timechain04@gmail.com>
2023-06-16 15:16:42 +0300

    pages

--- src/js/country-list-card.js
+++ src/js/country-list-card.js
@@ -1,4 +1,4 @@
-export default function createCountryListCard({ flags, name }) {
+export default function createCountryListCard({ flags, name })  {
     return `<li class='country-card'>
               <div class='country-img'><img src='${flags.svg}' alt='${name.official}'/></div>
               <p class='country-name'>${name.common}</p>
